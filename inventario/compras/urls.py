a48e83327a855f277098fd17ece6aa77386b049e
Hernan <hernandicampus@gmail.com>
2025-03-31 09:58:19 +0200

    Eliminar archivos que ya no son necesarios

--- inventario/compras/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.compra_list, name='compra_list'),
-    path('create/', views.compra_create, name='compra_create'),
-    path('edit/<int:pk>/', views.compra_edit, name='compra_edit'),
-    path('delete/<int:pk>/', views.compra_delete, name='compra_delete'),
-]
--- inventario/ventas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Venta
-
-class VentaForm(forms.ModelForm):
-    class Meta:
-        model = Venta
-        fields = ['producto', 'cantidad']
